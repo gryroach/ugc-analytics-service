a8b8b9f3258dfd98ba0520faba1bd0d1c0370865
sgavrilenko <sg@smena.space>
2025-01-22 21:12:32 +0300

    UGC-2: изменение типов и добавление полей в скрипт переноса данных в ClickHouse; преобразование полей с типом datetime

--- event_api/src/schemas/events.py
+++ event_api/src/schemas/events.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 from typing import Generic, TypeVar
 
-from pydantic import BaseModel, Field, PrivateAttr, computed_field
+from pydantic import (
+    BaseModel,
+    Field,
+    PrivateAttr,
+    computed_field,
+    field_validator,
+)
 
 T = TypeVar("T")
 
@@ -59,6 +65,15 @@ class BaseEvent(BaseModel):
     @property
     def operating_system(self) -> str:
         return self._operating_system
+
+    @field_validator(
+        "timestamp", "session_start_time", "session_end_time", mode="after"
+    )
+    @classmethod
+    def remove_timezone(cls, value: datetime) -> datetime:
+        """Удаление временной зоны для сохранения в ClickHouse"""
+        return value.replace(tzinfo=None)
+
 
 class SignUpEvent(BaseEvent):
     _event_type: str = PrivateAttr(default_factory=lambda: "sing up")
@@ -144,6 +159,7 @@ class ViewMovieDetailsEvent(BaseEvent):
     _event_type: str = PrivateAttr(default_factory=lambda: "view movie details")
     movie_id: str
     movie_title: str
+
 
 class EventResult(BaseModel, Generic[T]):
     result: str = Field(default="Event received")
